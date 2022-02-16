16cc37e27e6977c69cbaf05dae106b96a7edf846
Joshjwood <joshjwood91@gmail.com>
2022-02-16 01:31:46 +0000

    init

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 import search_function
-import smtplib
 import time
 
-my_email = "jwood.droid@gmail.com"
+sender_email = "jwood.droid@gmail.com"
 password = "Ii3Jy4Masr7z"
 
-my_main_email = "emerica4u@hotmail.com"
+receiver_email = "emerica4u@hotmail.com"
 
 NUMBER_OF_RESULTS = 20
-query_strings = ["New Fragrance Release", "Discontinued Fragrance"]
+query_strings = ["Fragrance", "New Fragrance Release", "Discontinued Fragrance"]
 
 for i in range(0, len(query_strings)):
-    email_text = search_function.run_a_search(query_strings[i], NUMBER_OF_RESULTS)
+    text = search_function.run_a_search(query_strings[i], NUMBER_OF_RESULTS)
+    html = search_function.run_a_search_html(query_strings[i], NUMBER_OF_RESULTS)
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f'{query_strings[i]} Watchdog'
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    message.attach(part1)
+    message.attach(part2)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
+    print(f"{[i + 1]} email(s) sorted.")
+    time.sleep(5)
 
 
-    message = f'Subject: {query_strings[i]} Watchdog\n\n{email_text}'
-    connection = smtplib.SMTP("smtp.gmail.com")
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs=my_main_email, msg=message.encode("utf-8"))
-    connection.close()
-    time.sleep(10)
-    print(f'Sorted.')
--- search_function.py
+++ search_function.py
@@ -1,14 +1,76 @@
 from youtubesearchpython import *
+from email_css import email_css
+from string import capwords
 
-NUMBER_OF_RESULTS = 20
+#NUMBER_OF_RESULTS = 20
 def run_a_search(query_term, number_of_results):
-    customSearch = CustomSearch(query_term, VideoSortOrder.uploadDate, limit = NUMBER_OF_RESULTS, region = "EU")
+    customSearch = CustomSearch(query_term, VideoSortOrder.uploadDate, limit = number_of_results, region = "EU")
     data = customSearch.result()
     email_text = ""
 
-    for i in range(0, NUMBER_OF_RESULTS):
-        email_text += f'{data["result"][i]["title"]}\n'
-        email_text += f'{data["result"][i]["link"]}\n'
-        email_text += f'{data["result"][i]["publishedTime"]}\n\n'
+    for i in range(0, number_of_results):
+        # Checking the view count, low view count videos are skipped/ignored
+        view_count_text = data["result"][i]["viewCount"]["text"]
+        view_count = ""
+        for n in range(0, len(view_count_text)):
+            if view_count_text[n].isnumeric():
+                view_count += str(view_count_text[n])
+            else:
+                pass
+        if view_count == "":
+            pass
+        elif int(view_count) < 2000:
+            pass
+        elif "Schar" in data["result"][i]["title"]:
+            pass
+        else:
+            email_text += f'{data["result"][i]["title"]}\n'
+            email_text += f'{data["result"][i]["link"]}\n'
+            email_text += f'{data["result"][i]["publishedTime"]}\n\n'
 
+    return email_text
+
+def run_a_search_html(query_term, number_of_results):
+    # Call original creators function with custom parameters
+    customSearch = CustomSearch(query_term, VideoSortOrder.uploadDate, limit = number_of_results, region = "EU")
+    data = customSearch.result()
+    email_text = ""
+    email_text = email_css
+
+
+    for i in range(0, number_of_results):
+        # Checking the view count, low view count videos are skipped/ignored
+        view_count_text = data["result"][i]["viewCount"]["text"]
+        view_count = ""
+        for n in range(0, len(view_count_text)):
+            if view_count_text[n].isnumeric():
+                view_count += str(view_count_text[n])
+            else:
+                pass
+        if view_count == "":
+            pass
+        elif int(view_count) < 500:
+            pass
+        elif "Schar" in data["result"][i]["title"]:
+            pass
+        ##############################################################
+        # Adding the image, title, link and view count
+        else:
+            image_ref = data["result"][i]["thumbnails"][0]["url"]
+            title_text = data["result"][i]["title"]
+            title_text = capwords(title_text)
+
+            email_text += f'<img src="{image_ref}" alt="{title_text}"><br>'
+
+            email_text += f'{title_text}<br>'
+            link_text = f'{data["result"][i]["link"]}'
+            email_text += f'<a href="{link_text}">Click to view</a>'
+            email_text += f' - {data["result"][i]["publishedTime"]}'
+            email_text += f' - Views: {data["result"][i]["viewCount"]["text"]}<br><br>'
+        ################################################################
+    # End HTML
+    email_text += """       </p>
+        </body>
+    </html>
+    """
     return email_text
